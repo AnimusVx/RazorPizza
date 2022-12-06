7eeb30f218f2f8e5f9dc2e4635c5496bacdfedff
AnimusVx <ulucmrce@gmail.com>
2022-12-06 15:01:37 +0300

    Migrations have been added. ProgramCS has been updated. Data modified. Model modified

--- RazorPizza/Data/PizzaDbContext.cs
+++ RazorPizza/Data/PizzaDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace RazorPizza.Data
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizza.Model;
+
+namespace RazorPizza.Data
 {
-    public class PizzaDbContext
+    public class PizzaDbContext : DbContext
     {
+        public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }  
     }
 }
