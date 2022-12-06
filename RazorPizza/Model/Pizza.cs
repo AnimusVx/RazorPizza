d0e4edc92efbd9cf5700f1246357a5d8a4d2ff8d
AnimusVx <ulucmrce@gmail.com>
2022-12-06 09:31:15 +0300

    Custom Pizza has been completed + Models were added.

--- RazorPizza/Model/Pizza.cs
+++ RazorPizza/Model/Pizza.cs
@@ -2,5 +2,11 @@
 {
     public class Pizza
     {
+        public string Name { get; set; }
+        public string ImageTitle { get; set; }
+        public float BasePrice { get; set; } = 10;
+        public bool Sauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
     }
 }
