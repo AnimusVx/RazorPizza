72fcc660557dc1eaf6e1024b5c588450ed86af78
AnimusVx <ulucmrce@gmail.com>
2022-12-06 11:26:42 +0300

    Checkout Page Model has been completed

--- RazorPizza/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizza/Pages/Checkout/Checkout.cshtml.cs
@@ -5,8 +5,22 @@ namespace RazorPizza.Pages.Checkout
 {
     public class CheckoutModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
         public void OnGet()
         {
+            if(string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "Custom Pizza";
+            }
+            if(string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
