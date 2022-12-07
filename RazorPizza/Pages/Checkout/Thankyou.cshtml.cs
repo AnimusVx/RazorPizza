1996f46c70e5206a3c6020fbd6a5f44283be5de4
AnimusVx <ulucmrce@gmail.com>
2022-12-07 10:15:18 +0300

    Database uploading has been added. After Confirm Order, the order is included in DB. +Migrations updated. +ConnectionString were adjusted.

--- RazorPizza/Pages/Checkout/Thankyou.cshtml.cs
+++ RazorPizza/Pages/Checkout/Thankyou.cshtml.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizza.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class ThankyouModel : PageModel
     {
+        public string Test { get; set; }
         public void OnGet()
         {
         }
